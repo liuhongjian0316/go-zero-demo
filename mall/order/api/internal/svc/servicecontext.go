73c9d2ea8783d0cbed33f5bec4a2f538a10d86bd
liuhongjian <liuhongjian0316@outlook.com>
2022-04-05 23:03:55 +0800

    init project

---
+++ mall/order/api/internal/svc/servicecontext.go
@@ -0,0 +1,19 @@
+package svc
+
+import (
+	"github.com/zeromicro/go-zero/zrpc"
+	"go-zero-demo/mall/order/api/internal/config"
+	"go-zero-demo/mall/user/rpc/user"
+)
+
+type ServiceContext struct {
+	Config config.Config
+	UserRpc user.User
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+		UserRpc: user.NewUser(zrpc.MustNewClient(c.UserRpc)),
+	}
+}
